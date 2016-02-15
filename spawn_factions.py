9b975fe3e17ea9593ccf0184d7858fa879af0ddf
Akuukis <redirected@kalvis.lv>
2016-02-15 14:58:04 +0200

    Make faction and initial colony spawning

--- spawn_factions.py
+++ spawn_factions.py
@@ -1,49 +1,62 @@
-def main(tick):
+import re
 
-    faction = {
-        'colonies': [colony, colony],
-        'pref': {  # 0 prefers first, 1 prefers last, 0.5 is indifferent.
-            'population_industry': 0.5,  # At what ratio (weighted by habitability and richness) to keep industry
-            'pacific_militaristic': 0.5,  # Size of warfleet to keep
-            'defence_attack': 0.5,  # Ratio of created warships to dedicate for defence
-            'growth_expand': 0.5,  # At what % of optimalPopulation to grow further or create settler
-        }
-    }
+import cp
+import utils
 
-    colony = {
-        'anchor': False,  # Planet, moon or orbit colony is attached to.
-        'id': 10000123,
-        'population': 1,
-        'industry': 1,
-        'ships': [ship, ship],
-        'storage': {
-            'goods': {
-                10000123: 100,
-                10000777: 100,
-                10000888: 100,
-                10000999: 100,
-            },
-            'solids': 50,  # Upkeep for industry
-            'metals': 30,  # For structure of skips
-            'isotopes': 20,  # For guns of ships
-            'ammo': 0  # For guns to shoot
-        }
-    }
+def main(tick, config, q):
+    # Delete 'empty' factions with no colonies
+    pass  # TODO
 
-    if random.random() > get_all_colonies().length / config.maxColonies:
-        picklist = []
-        for planet in get_all_planets():
-            distance = get_vector(planet.coords, planet.sun.coords)
-            moons = planet.moons.length
-            # 8 minutes and 1 moon gives highest chance (like Earth)
-            chance = moons / (moons * 2 + 0.0001)
-            chance -= math.fabs(config.optimalPlanetDistance - distance) / config.optimalPlanetRange
-            if chance > 0.01:
-                picklist.append([chance, planet])
-
-        lucky = choose_weighted(picklist)
-        faction = spawn_faction()
-        spawn_colony(faction, lucky)
+    # Spawn new factions with initial colony
+    count = int(cp.query(payload="SELECT * FROM massive WHERE object == 'faction' LIMIT 0, 0")['hits'])
+    want = int(utils.dist_skewedLeft(config['factions']))
+    if want > count:
+        planet = cp.query(payload="\
+            SELECT _id, size\
+            FROM massive\
+            WHERE\
+                object == 'planet' &&\
+                type != 'Gas' &&\
+                system_coords.radius < "+str(int(config['optimalDistance'][0]*config['optimalDistance'][1]))+" &&\
+                system_coords.radius > "+str(int(config['optimalDistance'][0]/config['optimalDistance'][1]))+"\
+            ORDER BY Math.abs("+str(config['optimalDistance'][0])+" - system_coords.radius) * Math.random()\
+            LIMIT 0, 1"
+            )["results"][0]
+        occupied = cp.query(payload="\
+            SELECT SUM(population+industry) as size\
+            FROM massive\
+            WHERE object == 'colony' && anchor == '"+planet['_id']+"'\
+            GROUP BY anchor")
+        if int(occupied['hits']) == 0 or (planet['size'] >= occupied['results'][0]['size'] + 2):
+            q.put(lambda a, b, c: cp.put(payload={
+                'object': 'faction',
+                'pref': {  # 0 prefers first, 1 prefers last, 0.5 is indifferent.
+                    'population_industry': utils.dist_flat(config['population_industry']),
+                    'pacific_militaristic': utils.dist_flat(config['pacific_militaristic']),
+                    'defence_attack': utils.dist_flat(config['defence_attack']),
+                    'growth_expand': utils.dist_flat(config['growth_expand']),
+                }},
+                params='[faction'+str(tick)+']',
+                msg='Spawn faction '+str(tick)
+            ))
+            q.put(lambda a, b, c: cp.put(payload={
+                'object': 'colony',
+                'faction': 'faction'+str(tick),
+                'anchor': planet['_id'],
+                'goods': re.search("(\w*)p", planet['_id']).group(1),
+                'population': 1,
+                'industry': 1,
+                'storage': {
+                    'goods': {
+                        re.search("(\w*)p", planet['_id']).group(1): 100,
+                    },
+                    'solids': 100,  # Upkeep for industry
+                    'metals': 0,  # For structure of skips
+                    'isotopes': 0,  # For guns of ships
+                    'ammo': 0  # For guns to shoot
+                }},
+                msg='Spawn colony'
+            ))
 
 if __name__ == "__main__":
     main()
