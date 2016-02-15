6cf6d3d1d71e8832cce44616c7014d4d477df487
Akuukis <akuukis@kalvis.lv>
2016-02-15 09:13:28 +0200

    Make universe growth & shrink adaptive

--- universe.py
+++ universe.py
@@ -24,75 +24,82 @@ def rndMaterials(solidsOther, MetalsIsotopes):
     return weights
 
 def main(tick, config, q):
-    universe = {
-        "tick": 0,
-        "systems": []
-    }
-
     # Workaround for scoping problems
     def workaround(payload, params, msg):
         q.put(lambda a, b, c: cp.put(payload=payload, params=params, msg=msg))
 
-    # Generate universe
-    pre = '[t' + str(universe['tick'])
-    for system in range(0, int(utils.dist_skewedLeft(config['systems']))):
-        urly = 'y' + str(system)
-        stars = []
-        for star in range(0, int(utils.dist_skewedLeft(config['stars']))):
-            urls = urly + 's' + str(star)
-            planets = []
-            for planet in range(0, int(utils.dist_skewedLeft(config['planets']))):
-                urlp = urls + 'p' + str(planet)
-                moons = []
-                for moon in range(0, int(utils.dist_skewedLeft(config['moons']))):
-                    urlm = urlp + 'm' + str(moon)
+    # How many systems are there?
+    count = int(cp.query(payload="SELECT * FROM massive WHERE object == 'system' LIMIT 0, 0")["hits"])
+
+    # How many we want?
+    want = int(utils.dist_skewedLeft(config['systems']))
+
+    if want == count:
+        pass
+    elif want >= count:
+        # Generate up to X more systems per tick
+        pre = '[t' + str(tick)
+        for system in range(0, min(config['systemsVolatility'], want - count)):
+            urly = 'y' + str(system)
+            stars = []
+            for star in range(0, int(utils.dist_skewedLeft(config['stars']))):
+                urls = urly + 's' + str(star)
+                planets = []
+                for planet in range(0, int(utils.dist_skewedLeft(config['planets']))):
+                    urlp = urls + 'p' + str(planet)
+                    moons = []
+                    for moon in range(0, int(utils.dist_skewedLeft(config['moons']))):
+                        urlm = urlp + 'm' + str(moon)
+                        workaround(payload={
+                                "id": moon,
+                                "object": "moon",
+                                "system_coords": rndCoords(config['moonDistance'], config['zFlatness']),
+                                'type': ['Gas', 'Ice', 'Rock', 'Iron', 'Mix'][utils.choose_weighted(config['moonType'])],
+                                'size': round(utils.dist_skewedLeft(config['moonSize']), 0),
+                                'habitability': round(utils.dist_skewedLeft(config['moonHabitability']), 0),
+                                'richness': round(utils.dist_skewedLeft(config['moonRichness']), 0),
+                                'materials': rndMaterials(config['moonWeightSolidsOther'], config['moonWeightMetalsIsotopes']),
+                            },
+                            params=pre+urlm+"]",
+                            msg='      Moon ' + pre+urlm+"]"
+                        )
                     workaround(payload={
-                            "id": moon,
-                            "object": "moon",
-                            "system_coords": rndCoords(config['moonDistance'], config['zFlatness']),
-                            'type': ['Gas', 'Ice', 'Rock', 'Iron', 'Mix'][utils.choose_weighted(config['moonType'])],
-                            'size': round(utils.dist_skewedLeft(config['moonSize']), 0),
-                            'habitability': round(utils.dist_skewedLeft(config['moonHabitability']), 0),
-                            'richness': round(utils.dist_skewedLeft(config['moonRichness']), 0),
-                            'materials': rndMaterials(config['moonWeightSolidsOther'], config['moonWeightMetalsIsotopes']),
+                            "id": planet,
+                            "object": "planet",
+                            "system_coords": rndCoords(config['planetDistance'], config['zFlatness']),
+                            "childs": moons,
+                            'type': ['Gas', 'Ice', 'Rock', 'Iron', 'Mix'][utils.choose_weighted(config['planetType'])],
+                            'size': round(utils.dist_skewedLeft(config['planetSize']), 0),
+                            'habitability': round(utils.dist_skewedLeft(config['planetHabitability']), 0),
+                            'richness': round(utils.dist_skewedLeft(config['planetRichness']), 0),
+                            'materials': rndMaterials(config['planetWeightSolidsOther'], config['planetWeightMetalsIsotopes']),
                         },
-                        params=pre+urlm+"]",
-                        msg='      Moon ' + urlm
+                        params=pre+urlp+"]",
+                        msg='    Planet ' + pre+urlp+"]"
                     )
                 workaround(payload={
-                        "id": planet,
-                        "object": "planet",
-                        "system_coords": rndCoords(config['planetDistance'], config['zFlatness']),
-                        "childs": moons,
-                        'type': ['Gas', 'Ice', 'Rock', 'Iron', 'Mix'][utils.choose_weighted(config['planetType'])],
-                        'size': round(utils.dist_skewedLeft(config['planetSize']), 0),
-                        'habitability': round(utils.dist_skewedLeft(config['planetHabitability']), 0),
-                        'richness': round(utils.dist_skewedLeft(config['planetRichness']), 0),
-                        'materials': rndMaterials(config['planetWeightSolidsOther'], config['planetWeightMetalsIsotopes']),
+                        "id": star,
+                        "object": "star",
+                        "system_coords": rndCoords(config['starDistance'], config['zFlatness']),
+                        "childs": planets
                     },
-                    params=pre+urlp+"]",
-                    msg='    Planet ' + urlp
+                    params=pre+urls+"]",
+                    msg='  Star ' + pre+urls+"]"
                 )
             workaround(payload={
-                    "id": star,
-                    "object": "star",
-                    "system_coords": rndCoords(config['starDistance'], config['zFlatness']),
-                    "childs": planets
-                },
-                params=pre+urls+"]",
-                msg='  Star ' + urls
+                "id": system,
+                "object": "system",
+                "universe_coords": {
+                    "x": random.randrange(config['x'][0],config['x'][1]),
+                    "y": random.randrange(config['y'][0],config['y'][1]),
+                    "z": random.randrange(config['z'][0],config['z'][1]),
+                },},
+                params=pre+urly+"]",
+                msg='System ' + pre+urly+"]"
             )
-        workaround(payload={
-            "id": system,
-            "object": "system",
-            "universe_coords": {
-                "x": random.randrange(config['x'][0],config['x'][1]),
-                "y": random.randrange(config['y'][0],config['y'][1]),
-                "z": random.randrange(config['z'][0],config['z'][1]),
-            },},
-            params=pre+urly+"]",
-            msg='System ' + urly
-        )
+    elif want <= count:
+        pass  # TODO destroy systems
+
     return "done"
 
 if __name__ == "__main__":
